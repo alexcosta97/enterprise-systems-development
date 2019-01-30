0cc4b77f5b42dd8fc313016247ce0151236f1967
Alexandre Costa <alex.callum.c@gmail.com>
2019-01-30 01:36:04 +0000

    feat: #37 set up AWS SDK

--- api/config/env/test.js
+++ api/config/env/test.js
@@ -3,5 +3,7 @@ module.exports = {
     db: process.env.MONGOURI || 'mongodb://localhost:27017/360-rooms',
     sessionSecret: process.env.sessionSecret || 'testSessionSecret',
     jwtSecret: process.env.jwtSecret || 'jwtTestSecret',
-    emptyID: '507f1f77bcf86cd799439011'
+    emptyID: '507f1f77bcf86cd799439011',
+    awsAccessKeyID: process.env.awsAccessKeyID || 'your AWS Access Key ID Here',
+    awsSecretAccessKey: process.env.awsSecretAccessKey || 'your AWS Access Key Here'
 }
