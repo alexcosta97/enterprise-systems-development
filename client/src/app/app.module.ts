9e9734669f55f598f88e7921c05c776703c0ff3c
Alexandre Costa <alex.callum.c@gmail.com>
2019-02-07 09:51:45 +0000

    feat: auto-generate components

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -5,10 +5,28 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { LoginService } from './services/login.service';
 import { JwtModule } from '@auth0/angular-jwt';
+import { UserComponent } from './user/user.component';
+import { PropertiesComponent } from './properties/properties.component';
+import { PropertyComponent } from './property/property.component';
+import { FloorsComponent } from './floors/floors.component';
+import { RoomsComponent } from './rooms/rooms.component';
+import { ShowRoomComponent } from './show-room/show-room.component';
+import { AuthComponent } from './auth/auth.component';
+import { NavbarComponent } from './navbar/navbar.component';
+import { HomeComponent } from './home/home.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    UserComponent,
+    PropertiesComponent,
+    PropertyComponent,
+    FloorsComponent,
+    RoomsComponent,
+    ShowRoomComponent,
+    AuthComponent,
+    NavbarComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule,
