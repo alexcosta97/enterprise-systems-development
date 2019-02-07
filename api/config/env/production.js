7807063f47004fca4110630f6631101bb8612810
Alexandre Costa <alex.callum.c@gmail.com>
2019-02-07 09:37:50 +0000

    fix: update env variable for db

--- api/config/env/production.js
+++ api/config/env/production.js
@@ -1,6 +1,6 @@
 module.exports = {
     //Development configuration options
-    db: process.env.MONGOURI || 'mongodb://localhost:27017/360-rooms',
+    db: process.env.MONGODB_URI || 'mongodb://localhost:27017/360-rooms',
     sessionSecret: process.env.sessionSecret || 'developmentSessionSecret',
     jwtSecret: process.env.jwtSecret || 'jwtDevSecret'
 }
