f8b0df2bc2454d7293fe208a99ab095c002bcb74
Alexandre Costa <alex.callum.c@gmail.com>
2019-02-07 15:01:21 +0000

    fix: fix tests by adding components

--- client/src/app/home/home.component.spec.ts
+++ client/src/app/home/home.component.spec.ts
@@ -1,6 +1,7 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { HomeComponent } from './home.component';
+import { PropertiesComponent } from '../properties/properties.component';
 
 describe('HomeComponent', () => {
   let component: HomeComponent;
@@ -8,7 +9,8 @@ describe('HomeComponent', () => {
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [ HomeComponent ]
+      declarations: [ HomeComponent,
+      PropertiesComponent ]
     })
     .compileComponents();
   }));
